8ef629d8063f55a3dd892df6be24d7a2735bdb30
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-31 23:09:23 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -16,13 +16,13 @@ def greeting(greeting, name)
 end
 
 def return_a_value
-puts "Nice"
+  puts "Nice"
 end
 
 def last_evaluated_value
-
+  puts "expert"
 end
 
-def pizza_party
-
+def pizza_party(value = "cheese")
+  value
 end
